cbdb1c960a6bc0aa4da9814dfcae50b8591b31d4
Pham Ngoc Phi <phiphongmta@gmail.com>
2017-10-08 13:54:09 +0700

    some pages

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyDqIRYOvxFI4A3rWEACvIOth8BuOiuUYAE",
+    authDomain: "fbcrududemy01.firebaseapp.com",
+    databaseURL: "https://fbcrududemy01.firebaseio.com",
+    projectId: "fbcrududemy01",
+    storageBucket: "fbcrududemy01.appspot.com",
+    messagingSenderId: "468935246195"
+}
---
+++ src/pages/add-shopping/add-shopping.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-add-shopping',
+  templateUrl: 'add-shopping.html',
+})
+export class AddShoppingPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AddShoppingPage');
+  }
+
+}
